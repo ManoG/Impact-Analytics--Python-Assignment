4651ffe19621663e0bf6c0bfbc5b3294a0e53af3
Mano Gnanaseelan <manognanaseelan17@gmail.com>
2023-06-23 03:38:22 +0530

    updated the comment

--- assignment.py
+++ assignment.py
@@ -1,6 +1,6 @@
 def solution(n):
     dp_ways = [1, 2, 4, 8] + [0] * (n - 3)          # Number of ways to attend on nth day
-    dp_miss = [0, 1, 2, 4] + [0] * (n - 3)          # Missing graduation on nth day
+    dp_miss = [0, 1, 2, 4] + [0] * (n - 3)          # Missing graduation for n classes
 
     for i in range(4, n + 1):
         dp_ways[i] = dp_ways[i - 1] + dp_ways[i - 2] + dp_ways[i - 3] + dp_ways[i - 4]
